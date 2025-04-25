a66d19b8a35afc5ca07d3e1fafad8d9b5a3ba5f4
mist941 <statkevich.ivan999@gmail.com>
2025-04-25 11:13:25 +0300

    Added several plugins for better expirience with vim

--- vim-setup.sh
+++ vim-setup.sh
@@ -2,19 +2,38 @@
 
 set -euo pipefail
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cat <<EOF > /home/$USER/.vimrc
 " Common adjustments
-set nocompatible 		" Disable vi compatibility mode
-set number			" Show line numbers
-set relativenumber		" Show relative line numbers
-syntax on			" Turn on syntax highlighting
-set tabstop=4			" 4 spaces for tabs
-set shiftwidth=4		" 4 spaces for auto indents
-set expandtab			" Replace tabs with spaces
-set autoindent			" Auto indents
-set smartindent			" Smart indents
-set cursorline			" Highlight current line
-set wildmenu			" Enable suggestions
+set nocompatible            " Turn off compatible with vi mod
+set number			        " Show line numbers
+syntax on			        " Turn on syntax highlighting
+set tabstop=4			    " 4 spaces for tabs
+set shiftwidth=4		    " 4 spaces for auto indents
+set expandtab			    " Replace tabs with spaces
+set autoindent			    " Auto indents
+set smartindent			    " Smart indents
+set cursorline			    " Highlight current line
+set wildmenu			    " Enable suggestions
 set clipboard=unnamedplus	" Sync with the system buffer
-set mouse=a			" Enable mouse
+set mouse=a			        " Enable mouse
+
+" Plugins
+call plug#begin('~/.vim/plugged')
+
+Plug 'morhetz/gruvbox'                          " Theme
+
+Plug 'neoclide/coc.nvim', {'branch': 'release'} " Autocomplete
+
+Plug 'preservim/nerdtree'                       " File manager
+
+Plug 'dense-analysis/ale'                       " Code analyses
+
+Plug 'tpope/vim-fugitive'                       " Git
+
+Plug 'vim-airline/vim-airline'                  " Status bar
+
+call plug#end()
 EOF
